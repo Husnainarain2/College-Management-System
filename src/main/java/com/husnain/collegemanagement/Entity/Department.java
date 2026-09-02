1255ccd2c95c3fee75b2886035073d70033519ac
Husnainarain2 <husnainabid9494@gmail.com>
2026-09-02 13:43:23 +0100

    Star a project of college management system

---
+++ src/main/java/com/husnain/collegemanagement/Entity/Department.java
@@ -0,0 +1,15 @@
+package com.husnain.collegemanagement.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Department {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
