a3c0e4549b7414140144e632b58a365a492f80a5
Husnainarain2 <husnainabid9494@gmail.com>
2026-09-04 11:05:21 +0100

    feat(pom.xml): add spring-orm dependency feat(Department.java): add @Id annotation feat(SecurityConfig.java): add SecurityConfig class feat(application.properties): add database configuration properties

--- src/main/java/com/husnain/collegemanagement/Entity/Department.java
+++ src/main/java/com/husnain/collegemanagement/Entity/Department.java
@@ -3,14 +3,14 @@ package com.husnain.collegemanagement.Entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+
 @Data
 @Entity
 public class Department {
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
